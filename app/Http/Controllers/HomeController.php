7ec032f9ea9b08dde74ff95f53aac3d0fa5df21f
Christian Mayer <christian@fox21.at>
2015-05-02 16:41:11 +0200

    Upcoming Birthdays added.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,12 @@
 <?php namespace App\Http\Controllers;
 
+use DateTime;
+
 use View;
+use Auth;
+use DB;
+
+use App\Person;
 
 class HomeController extends Controller{
 
@@ -30,7 +36,44 @@ class HomeController extends Controller{
 	 * @return Response
 	 */
 	public function index(){
-		$view = View::make('home');
+		$user = Auth::user();
+		$userId = $user->id;
+		
+		$now = new DateTime('now');
+		$now->setTime(0, 0, 0);
+		
+		$upcomingBirthdaysPersonsBuilder = Person::whereNull('deleted_at')
+			->where('user_id', '=', $userId)
+			->whereNotNull('birthday')
+			->where(DB::raw("date(concat(year(now()), '-', substring(birthday, 6)))"), '>=', DB::raw("str_to_date(now(), '%Y-%m-%d')"))
+			->select('*', DB::raw('substring(birthday, 6) as birthday_month_day'))
+			->orderBy('birthday_month_day', 'ASC')
+			->orderBy('last_name', 'ASC')
+			->orderBy('first_name', 'ASC')
+			->take(10);
+		$upcomingBirthdaysPersons = $upcomingBirthdaysPersonsBuilder->get();
+		
+		foreach($upcomingBirthdaysPersons as $personId => $person){
+			$birthday = new DateTime($person->birthday);
+			$birthdayThisYear = new DateTime($now->format('Y').'-'.$birthday->format('m-d'));
+			$diff = $birthdayThisYear->diff($now);
+			$person->diff = $diff->format('%R%a days');
+			
+			$diffInt = (int)$diff->format('%R%a');
+			if($diffInt == 0){
+				$person->diff = 'Today';
+			}
+			if($diffInt >= -14 && $diffInt <= 0){
+				$person->diff_color = '#006400';
+			}
+			elseif($diffInt < -14){
+				$person->diff_color = '#ff8c00';
+			}
+		}
+		
+		$view = View::make('home', array(
+			'upcomingBirthdaysPersons' => $upcomingBirthdaysPersons,
+		));
 		return $view;
 	}
 
