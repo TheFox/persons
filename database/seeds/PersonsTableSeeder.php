f7df0fb491fd8151e2b1f7adf513afa8197aeec7
Christian Mayer <christian@fox21.at>
2015-05-03 13:47:51 +0200

    Seeds added.

--- database/seeds/PersonsTableSeeder.php
+++ database/seeds/PersonsTableSeeder.php
@@ -26,6 +26,18 @@ class PersonsTableSeeder extends Seeder{
 			$tpl['first_met_at'] = '2005-01-01';
 			Person::create($tpl);
 		}
+		
+		for($i = 2000; $i < 2012; $i++){
+			$id++;
+			$tpl['last_name'] = 'last_name_'.$id;
+			$tpl['middle_name'] = 'middle_name_'.$id;
+			$tpl['first_name'] = 'first_name_'.$id;
+			$tpl['nick_name'] = 'nick_name_'.$id;
+			$tpl['birthday'] = '2000-02-21';
+			$tpl['first_met_at'] = $i.'-06-01';
+			Person::create($tpl);
+		}
+		
 	}
 	
 }
