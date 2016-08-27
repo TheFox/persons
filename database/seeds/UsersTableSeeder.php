54f15d3087eeabd1c2c475b770cbd9c0f66c3fd1
Christian Mayer <christian@fox21.at>
2016-08-27 08:45:27 +0200

    Set dev password to 'password'.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -9,9 +9,9 @@ class UsersTableSeeder extends Seeder{
 	public function run(){
 		DB::table('users')->delete();
 		
-		User::create(array('name' => 'root', 'email' => 'dev@fox21.at', 'password' => Hash::make('pass')));
-		User::create(array('name' => 'dev1', 'email' => 'dev1@fox21.at', 'password' => Hash::make('pass')));
-		User::create(array('name' => 'dev2', 'email' => 'dev2@fox21.at', 'password' => Hash::make('pass')));
+		User::create(array('name' => 'root', 'email' => 'dev@fox21.at', 'password' => Hash::make('password')));
+		User::create(array('name' => 'dev1', 'email' => 'dev1@fox21.at', 'password' => Hash::make('password')));
+		User::create(array('name' => 'dev2', 'email' => 'dev2@fox21.at', 'password' => Hash::make('password')));
 	}
 	
 }
