9ea7c287be819f6542b24345e2330b858dd26af8
Christian Mayer <christian@fox21.at>
2018-09-09 20:13:05 +0200

    Persons list. #14

--- src/PersonsBundle/Controller/PersonController.php
+++ src/PersonsBundle/Controller/PersonController.php
@@ -2,9 +2,21 @@
 
 namespace TheFox\PersonsBundle\Controller;
 
+use TheFox\PersonsBundle\Entity\Person;
+use TheFox\PersonsBundle\Form\PersonType;
+use TheFox\PersonsBundle\Repository\PersonRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class PersonController extends BaseController
 {
-    public function listAction()
+    /**
+     * @param PersonRepository $personRepository
+     * @return Response
+     */
+    public function indexAction(PersonRepository $personRepository): Response
     {
+        return $this->render('person/index.html.twig', ['people' => $personRepository->findAll()]);
     }
 }
